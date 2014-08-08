af95994d38b59f57f47dd1d21c4dc85a87d83a47
John Hoerr <jhoerr@iu.edu>
2014-08-08 11:50:56 -0400

    Fill out the Account model.
    Experimentally derived due to incomplete API documentation.

--- Canvas.v1/Models/Account.cs
+++ Canvas.v1/Models/Account.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Canvas.v1.Models
 {
@@ -7,6 +8,12 @@ namespace Canvas.v1.Models
     /// </summary>
     public class Account
     {
+        /// <summary>
+        /// The ID of the account
+        /// </summary>
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; private set; }
+
         /// <summary>
         /// The name of the account
         /// </summary>
@@ -36,5 +43,32 @@ namespace Canvas.v1.Models
         /// </summary>
         [JsonProperty(PropertyName = "default_group_storage_quota_mb")]
         public string DefaultGroupStorageQuotaMB { get; private set; }
+
+        /// <summary>
+        /// The ID of the parent account
+        /// </summary>
+        [JsonProperty(PropertyName = "parent_account_id")]
+        public string ParentAccountId { get; private set; }
+
+        /// <summary>
+        /// The ID of the root account
+        /// </summary>
+        [JsonProperty(PropertyName = "root_account_id")]
+        public string RootAccountId { get; private set; }
+
+        /// <summary>
+        /// The workflow state of the account
+        /// </summary>
+        [JsonProperty(PropertyName = "workflow_state")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public AccountWorkflowState WorkflowState { get; private set; }
+
+    }
+
+    public enum AccountWorkflowState
+    {
+        Active,
+        Completed,
+        All
     }
 }
