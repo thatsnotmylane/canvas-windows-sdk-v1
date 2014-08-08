f1abd4475be61cb1a224ceae9e214ee1bc2492d8
John Hoerr <jhoerr@iu.edu>
2014-08-08 12:01:43 -0400

    Add AccountsManager.GetAll deserialization test.

---
+++ Canvas.v1.Test/AccountsManagerTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Canvas.v1.Managers;
+using Canvas.v1.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Canvas.v1.Test
+{
+    [TestClass]
+    public class AccountsManagerTests : ResourceManagerTest
+    {
+        private readonly AccountsManager _accountsManager;
+
+        public AccountsManagerTests()
+        {
+            _accountsManager = new AccountsManager(_config.Object, _service, _converter, _authRepository);
+        }
+
+        [TestMethod]
+        public async Task GetAllAccounts()
+        {
+            const string contentString = @"[{""id"":1234,""name"":""Indiana University"",""parent_account_id"":null,""root_account_id"":null,""workflow_state"":""active"",""default_storage_quota_mb"":500,""default_user_storage_quota_mb"":100,""default_group_storage_quota_mb"":300,""default_time_zone"":""America/New_York""}]";
+
+            ArrangeSuccessfulResponse<IEnumerable<Account>>(contentString);
+
+            IEnumerable<Account> accounts = await _accountsManager.GetAll();
+
+            Assert.AreEqual(accounts.Count(), 1);
+            Account account = accounts.Single();
+            Assert.AreEqual(account.Id, "1234");
+            Assert.AreEqual(account.Name, "Indiana University");
+            Assert.AreEqual(account.ParentAccountId, null);
+            Assert.AreEqual(account.RootAccountId, null);
+            Assert.AreEqual(account.WorkflowState, AccountWorkflowState.Active);
+            Assert.AreEqual(account.DefaultStorageQuotaMB, 500);
+            Assert.AreEqual(account.DefaultUserStorageQuotaMB, 100);
+            Assert.AreEqual(account.DefaultGroupStorageQuotaMB, 300);
+            Assert.AreEqual(account.DefaultTimeZone, "America/New_York");
+        }
+    }
+}
--- Canvas.v1.Test/ResourceManagerTest.cs
+++ Canvas.v1.Test/ResourceManagerTest.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Canvas.v1.Auth;
 using Canvas.v1.Config;
 using Canvas.v1.Converter;
 using Canvas.v1.Models;
 using Canvas.v1.Request;
 using Canvas.v1.Services;
+using Canvas.v1.Wrappers;
+using Canvas.v1.Wrappers.Contracts;
 using Moq;
 
 namespace Canvas.v1.Test
@@ -29,6 +32,16 @@ namespace Canvas.v1.Test
             _config = new Mock<ICanvasConfig>();
 
             _authRepository = new AuthRepository(_config.Object, _service, _converter, new OAuth2Session("fakeAccessToken", "fakeRefreshToken", 3600, "bearer"));
+        }
+
+        protected void ArrangeSuccessfulResponse<T>(string contentString) where T: class 
+        {
+            _handler.Setup(h => h.ExecuteAsync<T>(It.IsAny<IApiRequest>()))
+                .Returns(() => Task.FromResult<IApiResponse<T>>(new ApiResponse<T>
+                {
+                    Status = ResponseStatus.Success,
+                    ContentString = contentString
+                }));
         }
     }
 }
